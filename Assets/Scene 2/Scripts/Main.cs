4ed2a43ee475855e8ad6d2b3caffed56b7501e37
Ariel <tahmaniak@gmail.com>
2017-03-27 20:42:46 -0400

    Scene2 part1

---
+++ Assets/Scene 2/Scripts/Main.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour {
+
+    public Sentence[] sentences;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Start!");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
---
+++ Assets/Scene 2/Scripts/Timer.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleTimer : MonoBehaviour
+{
+    public float targetTime = 0;
+
+    SimpleTimer(float targetTime)
+    {
+        this.targetTime = targetTime;
+    }
+
+    void Update()
+    {
+        if (targetTime <= 0.0f)
+        {
+
+        }
+
+        targetTime -= Time.deltaTime;
+
+        if (targetTime <= 0.0f)
+        {
+            timerEnded();
+        }
+
+    }
+
+    void timerEnded()
+    {
+        
+    }
+
+}
